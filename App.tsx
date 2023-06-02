542806908eb2ab907b29c859669c4d4fdbbd4646
Marcos Sarges <dev.marcos.sarges@gmail.com>
2023-06-02 10:30:16 -0300

    teste

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default App;
